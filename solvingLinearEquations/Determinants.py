d86fa2d5c25b3cd6a78e2625e9995d63e27b68c3
sarmiento-luevano <sarmientoluevano@gmail.com>
2024-10-01 16:34:25 -0600

    First version of finished Crammer's Method

--- solvingLinearEquations/Determinants.py
+++ solvingLinearEquations/Determinants.py
@@ -18,22 +18,73 @@ def det3(A):
 def inputMatrix():
     print("Write down the constants of each of the linear equations...")
     A = np.zeros((3,3))
+    b = np.zeros(3)
     for i in np.arange(3):
         print(f"{i+1}nd equation...")
         for j in np.arange(3):
             A[i,j] = (input(f"Enter the element for x_{j+1}: "))
+        b[i] = input(f"Enter the {i+1}nd constant: ")
 
     for i in range(3):
         print(f"{i+1}nd equation")
         for j in range(3):
-            print(f"{A[i,j]}x_{j+1}", end=' ')
-        print("")
+            print(f"{A[i,j]}x_{j+1} +", end=' ')
+        print(f" = {b[i]}")
 
-    return A
+    return A, b
+
+def crammerMatrix_kth(A,b,k):
+    n = A.shape[0]
+    B = np.zeros((n,n))
+    for i in range(n):
+        for j in range(n):
+            if (j == k):
+                B[i,j] = b[i]
+            else:
+                B[i,j] = A[i,j]
+    return B
+
+def iterateCrammer(n,A,b):
+    crammerMatrices = []
+    for k in range(n):
+        subMatrix = crammerMatrix_kth(A,b,k)
+        crammerMatrices.append(subMatrix)
+    return crammerMatrices
+
+def CrammerDeterminants_list(A,b):
+    n = A.shape[0]
+    matrices = iterateCrammer(n,A,b)
+    determinants = []
+    for matrix in matrices:
+        determinants.append(float(det3(matrix)))
+    return determinants
+    
+def CrammerSolutions(A,b):
+    detA = det3(A)
+    
+    if np.isclose(detA, 0):
+        return np.array([0])
+
+    lst = CrammerDeterminants_list(A,b)
+    solutions = np.array(lst)
+    solutions = solutions / detA
+    return solutions    
+    
 
 def main():
-    A = inputMatrix()
-    
+    A, b = inputMatrix()
+
+    solutions = CrammerSolutions(A,b)
+
+    if (solutions[0] == 0):
+        print("The system has no solutions")
+        return
+    print("The solutions are...")
+    for i in range(len(solutions)):
+        print(f"x_{i+1} = ", solutions[i])
+
+
+
 
 if __name__ == '__main__':
     main()
